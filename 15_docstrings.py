d0c6b2226e0f47719978ece838750072f76d576b
Md Nayeem <codernayeem898@gmail.com>
2021-04-23 19:38:15 +0600

    added docstrings

---
+++ 15_docstrings.py
@@ -0,0 +1,18 @@
+# docstring is making a documentation for your code to understand it easily
+""" This module converts pdf to image """
+
+
+def copy(data, count):
+    """
+    Copy a str to count times
+
+    Parameters:
+    data (str): The text that you want to copy
+    count (int): How much number of copy you want
+
+    Returns:
+    str: copied data
+    """
+    return data * count
+
+# Using pydoc, we can easily see these documentation for anything
