54ed10a53459a209847d24ab9a4e2df34e84df83
Md Nayeem <codernayeem898@gmail.com>
2021-04-25 19:51:25 +0600

    Updated classes with classmethod, staticmethod

--- 9_classes.py
+++ 9_classes.py
@@ -29,23 +29,97 @@ print(isinstance(point, Point)) # True
 print(isinstance(point, int))   # False
 
 
+
 class PointClass:
+    # class variable
     color = "Red"
+    apply_time = 5
+
     # constructor
     def __init__(self, x=0, y=0):
+        # instance variable
         self.x = x
         self.y = y
 
     def draw(self):
         print(self.x, self.y)
+    
+    
+    # this method change a instance variable
+    def change_color1(self, color):
+        self.color = color # this create a new instance variable
+
+    # this method change a class variable
+    @classmethod
+    def change_color2(cls, color):
+        cls.color = color  # this change class instance variable
 
 
 point = PointClass(4, 5)
-point.draw()
-PointClass.color = "Black" # this change will apply to all objects
-print(point.color)
-point.color = "Blue" # this change will apply to this object only
-print(point.color)
-point.z = 4       # we can also create new variables on that object
-print(point.z)
+print(point.color) # printing class variable
+
+# point.color = "Blue"
+# print(point.color) # printing instance variable
+
+# PointClass.color = "Black" # changing class variable
+point.change_color2('Yelow') # changing class variable
+print(point.color)  # printing class variable
+
+
+
+
+
+class Employee:
+    # constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = int(age)
+
+    # returns instance by reading given string (Alternative constructor)
+    @classmethod
+    def from_dash(cls, data):
+        return cls(*data.split('-'))
+    
+# using default constructor
+roni = Employee('Roni', 50)
+
+# using class method
+sami = Employee.from_dash('Sami-35')
+
+# use classmethod, when no need of instance variable, needed class variable
+
+
+
+
+class Coder:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def print_hi():
+        print('Welcome')
+
+    @staticmethod
+    def calculate(a, b):
+        return a * b
+
+# use staticmethod, when don't need any class/instance variables
+# when you don't need self/cls ...
+
+# calling from instance
+roni = Coder('Roni')
+roni.print_hi()
+
+# calling from class
+print(Coder.calculate(5, 9))
+
+
+
+# public variable/functons    : name
+# protected variable/functons : _name
+# private variable/functons   : __name
+
+# public    : use anywhere
+# protected : use in base and sub class
+# private   : use only in base class
 
